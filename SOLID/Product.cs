450d1e2e976af999825384b19de4ba3ede52b027
Alex Kiselov <dp.prk.kiselov@gmail.com>
2018-11-19 01:28:30 +0200

    init commit

---
+++ SOLID/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class Product
+    {
+        public Product(string name, Color color, Size size)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = Size;
+        }
+
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+    }
+
+    public enum Color
+    {
+        Blue, Green, Red, Yellow
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+}
---
+++ SOLID/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    class Program
+    {
+        public interface ISpecification<T>
+        {
+            bool IsSatisfied(T t);
+        }
+
+        public interface IFilter<T>
+        {
+            IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+        }
+
+        public class ColorSpecification: ISpecification<>
+
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+            Product[] products = { apple, tree, house };
+        }
+    }
+}
